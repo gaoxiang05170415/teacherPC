2659d215402cd0a3298734c69dd09c809abf079a
gaoxiang05170415 <gaoxiang05170415@163.com>
2022-02-21 14:03:52 +0800

    第一次提交

---
+++ pages/login/index.vue
@@ -0,0 +1,112 @@
+<template>
+	<view>
+		<view class="body">
+			<image src="../../image/background.png" mode="" class="backgtoundImage"></image>
+			<view class="bodyForm">
+				<image src="../../image/logo.png" mode="" style="width: 100%;height: 94rpx;"></image>
+				<view class="account">
+					<input type="text" value="" v-model="account" placeholder="账号" placeholder-style="color:#B6B6B6;opacity:0.5"/>
+				</view>
+				<view class="password">
+					<input type="text" value="" v-model="password" placeholder="密码" password="true" placeholder-style="color:#B6B6B6;opacity:0.5"/>
+				</view>
+				<view class="login" @click="login">
+					登&nbsp;&nbsp;&nbsp;录
+				</view>
+				<view class="formBottom">
+					<text class="register">新用户注册</text>
+					<text>|</text>
+					<text class="forgotPwd">忘记密码</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				account:'',
+				password:''
+			}
+		},
+		methods:{
+			login(){
+				uni.request({
+					url:'http://outuser.choor.cn/out_login_api/login/doLogin',
+					data:{
+						"account":this.account,
+						"password":this.password
+					},
+					method:"post",
+					success:res => {
+						console.log(res);
+						if(res.data.code == 0){
+							uni.redirectTo({
+								url:'../home/index'
+							})
+						}
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.body{
+		width: 100vw;
+		height: 94vh;
+		position: relative;
+	}
+	.backgtoundImage{
+		width: 100%;
+		height: 100%;
+	}
+	.bodyForm{
+		width: 70%;
+		height: 600rpx;
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		margin: auto;
+	}
+	.account,.password{
+		width: 100%;
+		height: 74rpx;
+		margin-top: 54rpx;
+		box-shadow: 5px 8px 53px 0px rgba(188, 222, 247, 0.65);
+	}
+	input{
+		width: 100%;
+		height: 100%;
+	}
+	.login{
+		width: 100%;
+		height: 74rpx;
+		background: linear-gradient(90deg, #46A8F1, #2B71FF);
+		border-radius: 12rpx;
+		font-size: 27rpx;
+		font-weight: bold;
+		color: #FFFFFF;
+		text-align: center;
+		line-height: 74rpx;
+		margin-top: 45rpx;
+	}
+	.formBottom{
+		width: 100%;
+		text-align: center;
+		color: #009BFF;
+		font-size: 22rpx;
+		margin-top: 26rpx;
+	}
+	.register{
+		padding-right: 30rpx;
+	}
+	.forgotPwd{
+		padding-left: 30rpx;
+	}
+</style>
